04ac4c726ba9747d3a632bba7d87bb09d9432307
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 13:31:10 -0500

    Refactor openapi-generator.sh to generate TypeScript-Axios client in src/openapi

--- app-v1/openapi-generator.sh
+++ app-v1/openapi-generator.sh
@@ -3,19 +3,4 @@
 content=$(curl -L http://localhost:8080/openapi.json)
 echo $content > openapi-schema.json
 
-# rm -rf openapi
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -g typescript-axios -i /local/openapi-schema.json -o /local/openapi
-
-# rm -rf src/app/openapi
-# mkdir -p src/app/openapi
-
-# cp -r openapi/api src/app/openapi/api
-# cp -r openapi/model src/app/openapi/model
-
-# cp openapi/param.ts src/app/openapi
-# cp openapi/encoder.ts src/app/openapi
-# cp openapi/variables.ts src/app/openapi
-# cp openapi/configuration.ts src/app/openapi
-
-# rm -rf openapi
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -g typescript-axios -i /local/openapi-schema.json -o /local/src/openapi
