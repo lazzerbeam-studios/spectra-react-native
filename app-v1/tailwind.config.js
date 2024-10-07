31742ed2eede83806958186b5b983e3b12249bd2
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-06 23:32:42 -0500

    Refactor tailwind.config.js to include file paths for app and components

--- app-v1/tailwind.config.js
+++ app-v1/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [
+    require('nativewind/preset'),
+  ],
   theme: {
     colors: {
       transparent: 'transparent',
