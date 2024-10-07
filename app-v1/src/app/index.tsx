a7946065091afbc2507e2cadb126fc6e1d686a1e
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 12:13:10 -0500

    Refactor index.tsx to update import order and remove unnecessary whitespace

--- app-v1/src/app/index.tsx
+++ app-v1/src/app/index.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { useStore } from '../stores/profile.store';
 
