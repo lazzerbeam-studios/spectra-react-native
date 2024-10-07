55f5346e99a689ee9aea59cfb54817f16efd22d6
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 12:25:19 -0500

    Refactor profile component to update import order and remove unnecessary whitespace

---
+++ app-v1/src/app/profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const SignIn = () => {
+  return (
+    <View>
+      <View className='flex-1 justify-center items-center'>
+        <Text className='text-lg text-bubble-gum'>profile</Text>
+      </View>
+    </View>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({});
