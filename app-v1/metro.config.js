aac9de91b5593fe177acbcc832b0337eabe25d8e
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 12:45:00 -0500

    Refactor import paths and filenames for global CSS files

--- app-v1/metro.config.js
+++ app-v1/metro.config.js
@@ -3,4 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './src/app/global.css' });
+module.exports = withNativeWind(config, { input: './src/app/_global.css' });
