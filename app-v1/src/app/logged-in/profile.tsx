f194e7cbe7b414984aee9ef7cc068f254f57dd1d
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 12:29:58 -0500

    Refactor profile component to update import order and remove unnecessary whitespace

--- app-v1/src/app/logged-in/profile.tsx
+++ app-v1/src/app/logged-in/profile.tsx
@@ -1,15 +1,36 @@
+import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
-const SignIn = () => {
+import { useStore } from '../../stores/profile.store';
+
+const Index = () => {
+  const { salmon, tuna, addSalmon, addTuna } = useStore();
+
   return (
-    <View>
-      <View className='flex-1 justify-center items-center'>
-        <Text className='text-lg text-bubble-gum'>profile</Text>
-      </View>
+    <View style={styles.page}>
+
+      <Text className='text-lg text-bubble-gum'>
+        Edit app/index.tsx to edit this screen. {salmon} {tuna}
+      </Text>
+
+      <TouchableOpacity onPressIn={() => addSalmon(1)}>
+        <Text>Add Salmon</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPressIn={() => addTuna(1)}>
+        <Text>Add Tuna</Text>
+      </TouchableOpacity>
+
     </View>
   );
 };
 
-export default SignIn;
+export default Index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
