dbf4ec2cbc709c8080a44d49e4dcf44ae9bbc1d8
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 13:40:09 -0500

    Refactor sign-in component to use petApi for fetching pet data

--- app-v1/src/app/(auth)/sign-in.tsx
+++ app-v1/src/app/(auth)/sign-in.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { petApi } from '../api';
+
 const SignIn = () => {
+
+  const [pet, setPet] = useState({});
+
+  const getThePet = async (id: number) => {
+    try {
+      const fetchedPet = await petApi.signUp({
+        email: 'ad',
+        password: '',
+      });
+      setPet(fetchedPet.data);
+    } catch (error) {
+      console.warn(error)
+    }
+  };
+
   return (
     <View>
       <View className='flex-1 justify-center items-center'>
