8d46f8fba13ead0824d2623e21c84c4385acec95
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-08 13:52:58 -0500

    Refactor home screen component: Update home.tsx to fix styling and structure

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -14,11 +14,11 @@ const HomeScreen = () => {
     //   <Text className="text-lg">Welcome to Home Screen</Text>
 
     // </View>
-    <View className="flex-1 flex-row">
+    <View className="flex flex-1 flex-row bg-purple">
 
-      <Text className="flex-1 text-bubble-gum" style={{ backgroundColor: 'red' }}>Yeah</Text>
-      <Text className="flex-1 text-bubble-gum" style={{ backgroundColor: 'darkorange' }}>Yeah</Text>
-      <Text className="flex-1 text-bubble-gum" style={{ backgroundColor: 'green' }}>Yeah</Text>
+      <Text className="text-bubble-gum" style={{ backgroundColor: 'red' }}>Yeah</Text>
+      <Text className="text-bubble-gum" style={{ backgroundColor: 'darkorange' }}>Yeah</Text>
+      <Text className="text-bubble-gum" style={{ backgroundColor: 'green' }}>Yeah</Text>
 
     </View>
 
