79e3893e29203479c801a93bf947aa0849644aa7
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-08 10:25:19 -0500

    Refactor home screen component in app-v1: Update index.tsx to use HomeScreen component

--- app-v1/src/app/index.tsx
+++ app-v1/src/app/index.tsx
@@ -1,24 +1,9 @@
-import { TouchableOpacity } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
-
-import { useStore } from '../stores/profile.store';
-
 import HomeScreen from '../screens/home';
 
 const Index = () => {
-  const { salmon, tuna, addSalmon, addTuna } = useStore();
-
   return (
-    <HomeScreen />
+    <HomeScreen></HomeScreen>
   );
 };
 
 export default Index;
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
