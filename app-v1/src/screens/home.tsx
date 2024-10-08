48b35b4c88bd76c656d778b880d1ad9a1be020a5
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-08 10:29:10 -0500

    Refactor home screen component in app-v1: Update home.tsx to add basic structure and styling

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -1,13 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>home</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg">Welcome to Home Screen</Text>
+      <Image
+        className="w-32 h-32 mt-4"
+        source={{ uri: 'https://example.com/your-image-url.png' }}
+      />
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+});
