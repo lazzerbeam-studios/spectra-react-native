0d733239c8a6eff129918b2ce2a9a3c5f92bc454
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-07 20:28:57 -0500

    Refactor api.ts to fix authorization header formatting

--- app-v1/src/app/api.ts
+++ app-v1/src/app/api.ts
@@ -1,9 +1,11 @@
-import { AuthApi, Configuration } from '../openapi';
 import axios from 'axios';
+import { AuthApi, Configuration } from '../openapi';
 
 const config = new Configuration();
 const axiosInstance = axios.create({
-  headers: { Authorization: 'YOUR_TOKEN' },
+  headers: {
+    Authorization: 'YOUR_TOKEN',
+  },
 });
 
 const authApi = new AuthApi(config, 'http://localhost:8080', axiosInstance);
