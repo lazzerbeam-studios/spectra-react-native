e235543c9d4c48a5bcaa3fbf4b8ec47a61473bc3
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-08 17:31:10 -0500

    Refactor global.css: Comment out dark.css import

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -15,9 +15,9 @@ const HomeScreen = () => {
 
     // </View>
     <View className="flex flex-1 flex-row bg-purple">
-      <Text className="text-bubble-gum" style={{ backgroundColor: 'red' }}>Yeah</Text>
-      <Text className="text-bubble-gum" style={{ backgroundColor: 'darkorange' }}>Yeah</Text>
-      <Text className="text-bubble-gum" style={{ backgroundColor: 'green' }}>Yeah</Text>
+      <Text className="text-primary" style={{ backgroundColor: 'red' }}>Yeah</Text>
+      <Text className="text-secondary" style={{ backgroundColor: 'darkorange' }}>Yeah</Text>
+      <Text className="text-typography" style={{ backgroundColor: 'green' }}>Yeah</Text>
     </View>
 
   );
