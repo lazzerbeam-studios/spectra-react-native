72e0999ef599b92e14944945f7cafe24c457a8f3
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-08 23:32:35 -0500

    Refactor api.ts: Update axiosInstance headers

--- app-v1/src/api.ts
+++ app-v1/src/api.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import { AuthApi, Configuration } from './openapi';
 
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
 const config = new Configuration();
+
 const axiosInstance = axios.create({
   headers: {
     Authorization: 'YOUR_TOKEN',
   },
 });
-
-const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 const authApi = new AuthApi(config, apiUrl, axiosInstance);
 
