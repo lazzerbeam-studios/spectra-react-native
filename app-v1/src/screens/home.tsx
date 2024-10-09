18419558d66e9d52ee642559eb0ff6a23c6980b2
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-08 20:09:25 -0500

    Refactor global.css: Comment out unused code and update color variables for primary and secondary

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -1,22 +1,18 @@
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-import '../global.css';
-
 const HomeScreen = () => {
 
   document.querySelector("html")?.setAttribute("data-theme", 'dark');
 
   return (
     // <View className="flex-1 justify-center items-center">
-
     //   <Image
     //     className="w-32 h-32 mt-4"
     //     source={require('../assets/images/react-logo.png')}
     //   />
-
     //   <Text className="text-lg">Welcome to Home Screen</Text>
-
     // </View>
+
     <View className="flex flex-1 flex-row bg-purple">
       <Text className="text-primary" style={{ backgroundColor: 'red' }}>Yeah</Text>
       <Text className="text-secondary" style={{ backgroundColor: 'darkorange' }}>Yeah</Text>
