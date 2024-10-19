3bd7be9d677b3e5c6b6c2720a478a9148d9c0871
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-19 00:48:13 -0500

    update

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -4,19 +4,9 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 const HomeScreen = () => {
 
   return (
-
-    // <View className='flex-1 justify-center items-center'>
-    //   <Image
-    //     className='w-32 h-32 mt-4'
-    //     source={require('../assets/images/react-logo.png')}
-    //   />
-    //   <Text className='text-lg'>Welcome to Home Screen</Text>
-    // </View>
-
     <SafeAreaView className="bg-background justify-center items-center flex-1 flex-col">
       <Text className='text-text'>Spectra</Text>
     </SafeAreaView>
-
   );
 };
 
