0f2ea77c34f4380f3a3932a914bdf8d899590bdb
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-19 00:52:53 -0500

    update

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -1,11 +1,17 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
 
   return (
-    <SafeAreaView className="bg-background justify-center items-center flex-1 flex-col">
-      <Text className='text-text'>Spectra</Text>
+    <SafeAreaView className='bg-background justify-center items-center flex-1 flex-col'>
+
+      <Text className='text-text text-4xl font-bold'>Spectra</Text>
+
+      <TouchableOpacity onPress={() => alert('Button Pressed!')}>
+        <Text className='text-text text-4xl font-bold'>Click Me</Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 };
