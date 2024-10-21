53372f26be11c5ec6549fd2a2c09e5a374919a4a
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-21 17:07:01 -0500

    Refactor babel.config.js in app-v1 to enable Tamagui plugin and update presets

--- app-v1/babel.config.js
+++ app-v1/babel.config.js
@@ -1,19 +1,22 @@
 module.exports = (api) => {
   api.cache(true)
   return {
-    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+    presets: [
+      [
+        'babel-preset-expo',
+        { jsxRuntime: 'automatic' },
+      ],
+    ],
     plugins: [
       [
         '@tamagui/babel-plugin',
         {
+          logTimings: true,
           components: ['tamagui'],
           config: './tamagui.config.ts',
-          logTimings: true,
           disableExtraction: process.env.NODE_ENV === 'development',
         },
       ],
-
-      // NOTE: this is only necessary if you are using reanimated for animations
       'react-native-reanimated/plugin',
     ],
   }
