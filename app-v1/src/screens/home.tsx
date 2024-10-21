e8d2e209c401529ff0b836c47c0d3eefb1636d44
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-20 22:38:23 -0500

    update

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -10,7 +10,7 @@ const HomeScreen = () => {
         Spectra
       </Text>
 
-      <Link href="/sign-up" push asChild>
+      <Link href='/sign-up' push asChild>
         <TouchableOpacity className='w-60 rounded-full border-2 border-text bg-primary hover:bg-background'>
           <Text className='p-4 text-center text-3xl font-semibold text-primarytext hover:text-text'>
             Sign Up
