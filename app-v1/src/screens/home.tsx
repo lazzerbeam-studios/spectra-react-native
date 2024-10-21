d9a749d607a73ff29f0ed42b9e44ae695b0217e1
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-21 00:15:10 -0500

    update

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'expo-router';
+import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Platform, Text, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
@@ -11,16 +11,16 @@ const HomeScreen = () => {
       </Text>
 
       <Link href='/sign-up' push asChild>
-        <TouchableOpacity className={`mb-6 w-60 rounded-full border-2 border-text bg-primary ${Platform.OS === 'web' ? 'hover:bg-background' : ''}`}>
-          <Text className={`p-4 text-center text-3xl font-semibold text-primarytext ${Platform.OS === 'web' ? 'hover:text-text' : ''}`}>
+        <TouchableOpacity className='mb-6 w-60 rounded-full border-2 border-text bg-primary'>
+          <Text className='p-4 text-center text-3xl font-semibold text-primarytext'>
             Sign Up
           </Text>
         </TouchableOpacity>
       </Link>
 
       <Link href='/sign-in' push asChild>
-        <TouchableOpacity className={`w-60 rounded-full border-2 border-text bg-primary ${Platform.OS === 'web' ? 'hover:bg-background' : ''}`}>
-          <Text className={`p-4 text-center text-3xl font-semibold text-primarytext ${Platform.OS === 'web' ? 'hover:text-text' : ''}`}>
+        <TouchableOpacity className='w-60 rounded-full border-2 border-text bg-primary'>
+          <Text className='p-4 text-center text-3xl font-semibold text-primarytext'>
             Sign In
           </Text>
         </TouchableOpacity>
