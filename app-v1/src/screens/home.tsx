6b3483e6a55532755053679a4a36a25ab142f0ba
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-20 22:37:07 -0500

    Refactor home screen: Remove unnecessary code and update sign up button styling

--- app-v1/src/screens/home.tsx
+++ app-v1/src/screens/home.tsx
@@ -3,7 +3,6 @@ import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
-
   return (
     <SafeAreaView className='flex flex-1 flex-col items-center justify-center bg-background'>
 
@@ -12,9 +11,7 @@ const HomeScreen = () => {
       </Text>
 
       <Link href="/sign-up" push asChild>
-        <TouchableOpacity
-          className='w-60 rounded-full border-2 border-text bg-primary hover:bg-background'
-        >
+        <TouchableOpacity className='w-60 rounded-full border-2 border-text bg-primary hover:bg-background'>
           <Text className='p-4 text-center text-3xl font-semibold text-primarytext hover:text-text'>
             Sign Up
           </Text>
