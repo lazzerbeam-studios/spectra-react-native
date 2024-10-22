95f9a2ca6c0fc37a73280522d13d92996902c662
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-21 23:41:54 -0500

    Refactor babel.config.js to add @tamagui/babel-plugin and react-native-reanimated/plugin

--- app-v1/babel.config.js
+++ app-v1/babel.config.js
@@ -4,5 +4,17 @@ module.exports = function (api) {
     presets: [
       'babel-preset-expo',
     ],
-  };
-};
+    plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  }
+}
