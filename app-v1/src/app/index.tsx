09af72aa324a3256f795c34c173553b9e06c8a65
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-22 00:43:49 -0500

    Refactor babel.config.js to use arrow function syntax

--- app-v1/src/app/index.tsx
+++ app-v1/src/app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native'
+import { Button, XGroup, XStack, YStack } from 'tamagui'
 
 export default function Index() {
   return (
     <View>
       <Text style={{ color: 'white' }}>index.tsx</Text>
+      <Button>Plain</Button>
     </View>
   )
 }
