4f78042d91de9cb25626648b119ae4a5622ac008
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-22 00:00:46 -0500

    Refactor layout and add Tamagui integration
    This commit refactors the layout component in the app and adds integration with Tamagui. It imports the necessary dependencies, including `TamaguiProvider` from 'tamagui', and `useColorScheme`, `DarkTheme`, and `DefaultTheme` from '@react-navigation/native'. It also adds the `tamaguiConfig` from '../../tamagui.config'. The layout component is wrapped in `TamaguiProvider` with the provided configuration and default theme based on the color scheme. The `ThemeProvider` is used to apply the dark or default theme based on the color scheme. Finally, the `index` screen is rendered within the `Stack` component.
    
    The `index` component is also modified to apply a white color to the text.
    
    No changes are made to the tamagui components configuration files or the tamagui web CSS file.

--- app-v1/src/app/_layout.tsx
+++ app-v1/src/app/_layout.tsx
@@ -1,9 +1,21 @@
+import '../../tamagui-web.css'
+
 import { Stack } from 'expo-router'
+import { TamaguiProvider } from 'tamagui'
+import { useColorScheme } from 'react-native'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+
+import { tamaguiConfig } from '../../tamagui.config'
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme()
   return (
-    <Stack>
-      <Stack.Screen name='index' />
-    </Stack>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name='index' />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   )
 }
--- app-v1/src/app/index.tsx
+++ app-v1/src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 export default function Index() {
   return (
     <View>
-      <Text>index.tsx</Text>
+      <Text style={{ color: 'white' }}>index.tsx</Text>
     </View>
   )
 }
