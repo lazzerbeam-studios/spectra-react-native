5d3cf249fc4906d3e185b844a5616f6ae51cec80
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-21 23:47:08 -0500

    Refactor metro.config.js to add @tamagui/babel-plugin, @tamagui/config, and @tamagui/metro-plugin

---
+++ app-v1/metro.config.js
@@ -0,0 +1,14 @@
+/** @type {import('expo/metro-config').MetroConfig} */
+
+const { getDefaultConfig } = require('expo/metro-config')
+const { withTamagui } = require('@tamagui/metro-plugin')
+
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+})
+
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui-web.css',
+})
