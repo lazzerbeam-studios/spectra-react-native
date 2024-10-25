53e2aa84442aa6c711ba100ae734df0ddad327ef
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-25 13:54:12 -0500

    Refactor HomeScreen component to use theme background color and update imports in index.tsx

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'expo-router';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { YStack, XStack, Text, useTheme, styled } from 'tamagui';
 
@@ -7,33 +8,26 @@ const HomeScreen = () => {
 
   const background = theme.background.get();
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: background,
+    }
+  })
+
   const Heading = styled(Text, {
     fontSize: 50,
-    color: '$blue10',
+    color: '$color',
     fontFamily: '$body',
   })
 
   return (
 
-    <SafeAreaView style={{
-      flex: 1,
-      backgroundColor: background,
-    }}>
+    <SafeAreaView style={styles.container}>
 
       <YStack flex={1} alignItems='center' justifyContent='center'>
-        <XStack backgroundColor='green'>
-
-          <Heading>Heading</Heading>
-
-        </XStack>
+        <Heading>Heading</Heading>
       </YStack>
-
-      {/* <YStack flex={1}>
-
-        <YStack alignItems='center' justifyContent='center'>
-
-        </YStack>
-      </YStack> */}
 
       {/* <Link href='/sign-up' push asChild>
         <H1 color={'$blue10'}>
