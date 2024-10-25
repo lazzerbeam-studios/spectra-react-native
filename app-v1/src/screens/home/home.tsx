9eb700742965dd1a8671f8942b1706f434955f05
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-25 13:57:16 -0500

    Refactor HomeScreen component: update imports and use theme background color

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
+import { YStack, Text, useTheme, styled } from 'tamagui';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { YStack, XStack, Text, useTheme, styled } from 'tamagui';
 
 const HomeScreen = () => {
   const theme = useTheme();
