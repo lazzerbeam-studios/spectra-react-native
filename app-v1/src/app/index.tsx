8a9a52c317429ad30844692d3218178ae19bdc20
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-24 23:09:33 -0500

    Refactor home screen component and update imports in index.tsx

--- app-v1/src/app/index.tsx
+++ app-v1/src/app/index.tsx
@@ -1,36 +1,7 @@
-import { Stack, Link } from 'expo-router';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-import { Button, SizableText, View } from 'tamagui';
-
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-
-import { useTheme } from 'tamagui';
-
-// import { Button } from '~/src/components/Button';
-// import { Container } from '~/src/components/Container';
-// import { ScreenContent } from '~/src/components/ScreenContent';
+import HomeScreen from '~/src/screens/home/home';
 
 export default function Home() {
-
-  const theme = useTheme();
-
-  const red = theme.red10.get();
-
-  console.log(red);
-
   return (
-
-
-    <SafeAreaView >
-
-    <View backgroundColor={"$background"}>
-      <SizableText color={"$blue10"} fontSize={24} fontWeight={300}>Yeah</SizableText>
-
-      {/* <Button>Yeah</Button> */}
-
-    </View>
-    </SafeAreaView>
-
+    <HomeScreen></HomeScreen>
   );
 }
--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,13 +1,20 @@
 import { Link } from 'expo-router';
 import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button, SizableText, View } from 'tamagui';
 
 const HomeScreen = () => {
   return (
 
-    <Text>
-      Spectra
-    </Text>
+    <SafeAreaView >
+      <Text>
+        Spectra
+      </Text>
+      <HomeScreen></HomeScreen>
+
+      <SizableText color={"$blue10"} fontSize={24} fontWeight={300}>Yeah</SizableText>
+
+    </SafeAreaView>
 
   );
 };
