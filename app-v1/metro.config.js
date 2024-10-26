205c7af6477f842f7a5d2abe1fae80e59c4855bc
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-26 00:06:12 -0500

    Refactor metro.config.js to include withNativeWind and update input path

--- app-v1/metro.config.js
+++ app-v1/metro.config.js
@@ -1,6 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './src/global.css' });
+module.exports = withNativeWind(config, {
+  input: './src/global.css',
+});
