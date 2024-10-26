d0a67ad9845a469397cc55a4d277331cef1ce7e2
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-26 08:48:30 -0500

    Refactor index.js to export App component and remove unnecessary imports

--- app-v1/src/index.js
+++ app-v1/src/index.js
@@ -1,9 +1,6 @@
-import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
+import { registerRootComponent } from 'expo';
 
-// https://docs.expo.dev/router/reference/troubleshooting/#expo_router_app_root-not-defined
-
-// Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context('./app');
   return <ExpoRoot context={ctx} />;
