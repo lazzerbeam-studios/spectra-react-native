bdb8d1e95be536d99973c42ebcfe7ff40faa9220
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-27 21:02:12 -0500

    Refactor package-lock.json, Sun.tsx, and tailwind.config.js

--- app-v3/src/index.js
+++
@@ -1,9 +0,0 @@
-import { ExpoRoot } from 'expo-router';
-import { registerRootComponent } from 'expo';
-
-export function App() {
-  const ctx = require.context('./app');
-  return <ExpoRoot context={ctx} />;
-}
-
-registerRootComponent(App);
