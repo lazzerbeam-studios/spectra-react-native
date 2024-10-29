0fa4b48fa931c467a29defab81b160827f1336d8
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 10:18:41 -0500

    Refactor import paths and order for consistency in _layout component and update profile screen

--- app-v1/src/screens/profile/profile.tsx
+++ app-v1/src/screens/profile/profile.tsx
@@ -27,71 +27,73 @@ const ProfileScreen = () => {
   }
 
   return (
-    <View className='flex-1 items-center justify-center gap-5 bg-secondary/30 p-6'>
-      <Card className='w-full max-w-sm rounded-2xl p-6'>
-        <CardHeader className='items-center'>
-          <Avatar alt="Rick Sanchez's Avatar" className='h-24 w-24'>
-            <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-            <AvatarFallback>
-              <Text>RS</Text>
-            </AvatarFallback>
-          </Avatar>
-          <View className='p-3' />
-          <CardTitle className='pb-2 text-center'>Rick Sanchez</CardTitle>
-          <View className='flex-row'>
-            <CardDescription className='text-base font-semibold'>Scientist</CardDescription>
-            <Tooltip delayDuration={150}>
-              <TooltipTrigger className='px-2 pb-0.5 active:opacity-50'>
-                <Info size={14} strokeWidth={2.5} className='h-4 w-4 text-foreground/70' />
-              </TooltipTrigger>
-              <TooltipContent className='px-4 py-2 shadow'>
-                <Text className='native:text-lg'>Freelance</Text>
-              </TooltipContent>
-            </Tooltip>
-          </View>
-        </CardHeader>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Dimension</Text>
-              <Text className='text-xl font-semibold'>C-137</Text>
+    <>
+      <View className='flex-1 items-center justify-center gap-5 bg-secondary/30 p-6'>
+        <Card className='w-full max-w-sm rounded-2xl p-6'>
+          <CardHeader className='items-center'>
+            <Avatar alt="Rick Sanchez's Avatar" className='h-24 w-24'>
+              <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
+              <AvatarFallback>
+                <Text>RS</Text>
+              </AvatarFallback>
+            </Avatar>
+            <View className='p-3' />
+            <CardTitle className='pb-2 text-center'>Rick Sanchez</CardTitle>
+            <View className='flex-row'>
+              <CardDescription className='text-base font-semibold'>Scientist</CardDescription>
+              <Tooltip delayDuration={150}>
+                <TooltipTrigger className='px-2 pb-0.5 active:opacity-50'>
+                  <Info size={14} strokeWidth={2.5} className='h-4 w-4 text-foreground/70' />
+                </TooltipTrigger>
+                <TooltipContent className='px-4 py-2 shadow'>
+                  <Text className='native:text-lg'>Freelance</Text>
+                </TooltipContent>
+              </Tooltip>
             </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Age</Text>
-              <Text className='text-xl font-semibold'>70</Text>
+          </CardHeader>
+          <CardContent>
+            <View className='flex-row justify-around gap-3'>
+              <View className='items-center'>
+                <Text className='text-sm text-muted-foreground'>Dimension</Text>
+                <Text className='text-xl font-semibold'>C-137</Text>
+              </View>
+              <View className='items-center'>
+                <Text className='text-sm text-muted-foreground'>Age</Text>
+                <Text className='text-xl font-semibold'>70</Text>
+              </View>
+              <View className='items-center'>
+                <Text className='text-sm text-muted-foreground'>Species</Text>
+                <Text className='text-xl font-semibold'>Human</Text>
+              </View>
             </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Species</Text>
-              <Text className='text-xl font-semibold'>Human</Text>
+          </CardContent>
+          <CardFooter className='flex-col gap-3 pb-0'>
+            <View className='flex-row items-center overflow-hidden'>
+              <Text className='text-sm text-muted-foreground'>Productivity:</Text>
+              <LayoutAnimationConfig skipEntering>
+                <Animated.View
+                  key={progress}
+                  entering={FadeInUp}
+                  exiting={FadeOutDown}
+                  className='w-11 items-center'
+                >
+                  <Text className='text-sm font-bold text-sky-600'>{progress}%</Text>
+                </Animated.View>
+              </LayoutAnimationConfig>
             </View>
-          </View>
-        </CardContent>
-        <CardFooter className='flex-col gap-3 pb-0'>
-          <View className='flex-row items-center overflow-hidden'>
-            <Text className='text-sm text-muted-foreground'>Productivity:</Text>
-            <LayoutAnimationConfig skipEntering>
-              <Animated.View
-                key={progress}
-                entering={FadeInUp}
-                exiting={FadeOutDown}
-                className='w-11 items-center'
-              >
-                <Text className='text-sm font-bold text-sky-600'>{progress}%</Text>
-              </Animated.View>
-            </LayoutAnimationConfig>
-          </View>
-          <Progress value={progress} className='h-2' indicatorClassName='bg-sky-600' />
-          <View />
-          <Button
-            variant='outline'
-            className='shadow shadow-foreground/5'
-            onPress={updateProgressValue}
-          >
-            <Text>Update</Text>
-          </Button>
-        </CardFooter>
-      </Card>
-    </View>
+            <Progress value={progress} className='h-2' indicatorClassName='bg-sky-600' />
+            <View />
+            <Button
+              variant='outline'
+              className='shadow shadow-foreground/5'
+              onPress={updateProgressValue}
+            >
+              <Text>Update</Text>
+            </Button>
+          </CardFooter>
+        </Card>
+      </View>
+    </>
   )
 }
 
