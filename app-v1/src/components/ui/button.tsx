58cbb4bb14c10ecd708767c13f079342f7d909c4
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-28 23:30:02 -0500

    Refactor import order for consistency in button component

--- app-v1/src/components/ui/button.tsx
+++ app-v1/src/components/ui/button.tsx
@@ -12,8 +12,7 @@ const buttonVariants = cva(
       variant: {
         default: 'bg-primary web:hover:opacity-90 active:opacity-90',
         destructive: 'bg-destructive web:hover:opacity-90 active:opacity-90',
-        outline:
-          'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
+        outline: 'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
         secondary: 'bg-secondary web:hover:opacity-80 active:opacity-80',
         ghost: 'web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
         link: 'web:underline-offset-4 web:hover:underline web:focus:underline ',
