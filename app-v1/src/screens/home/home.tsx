6e1d525411194df5cf2ff4458498e8b115996836
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 14:10:15 -0500

    Refactor import paths and order in NotFoundScreen component, and update Button component in NotFoundScreen

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,29 +1,31 @@
 import { Link } from 'expo-router';
-import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Text } from '~/src/components/ui/text';
+import { Button } from '~/src/components/ui/button';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView className='flex flex-1 flex-col items-center justify-center bg-background'>
 
-      <Text className='mb-32 text-8xl font-bold text-foreground'>
+      <Text className='mb-32 text-8xl font-bold'>
         Spectra
       </Text>
 
       <Link href='/sign-up' push asChild>
-        <TouchableOpacity className='border-text mb-6 w-60 rounded-full border-2 bg-primary'>
-          <Text className='text-primarytext p-4 text-center text-3xl font-semibold'>
+        <Button className='border-text mb-6 w-60 rounded-full border-2 bg-primary'>
+          <Text className='p-4 text-center text-3xl font-semibold'>
             Sign Up
           </Text>
-        </TouchableOpacity>
+        </Button>
       </Link>
 
       <Link href='/sign-in' push asChild>
-        <TouchableOpacity className='border-text w-60 rounded-full border-2 bg-primary'>
-          <Text className='text-primarytext p-4 text-center text-3xl font-semibold'>
+        <Button className='border-text w-60 rounded-full border-2 bg-primary'>
+          <Text className='p-4 text-center text-3xl font-semibold'>
             Sign In
           </Text>
-        </TouchableOpacity>
+        </Button>
       </Link>
 
     </SafeAreaView>
