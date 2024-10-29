bac01af05803b6979ed52939b08d7315e1c2affa
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-28 22:59:46 -0500

    Refactor import order for consistency in tooltip component

--- app-v1/src/components/ui/tooltip.tsx
+++ app-v1/src/components/ui/tooltip.tsx
@@ -1,18 +1,15 @@
-import * as TooltipPrimitive from '@rn-primitives/tooltip';
 import * as React from 'react';
 import { Platform, StyleSheet } from 'react-native';
+import * as TooltipPrimitive from '@rn-primitives/tooltip';
 import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
-import { TextClassContext } from '~/src/components/ui/text';
+
 import { cn } from '~/src/lib/utils';
+import { TextClassContext } from '~/src/components/ui/text';
 
 const Tooltip = TooltipPrimitive.Root;
-
 const TooltipTrigger = TooltipPrimitive.Trigger;
 
-const TooltipContent = React.forwardRef<
-  TooltipPrimitive.ContentRef,
-  TooltipPrimitive.ContentProps & { portalHost?: string }
->(({ className, sideOffset = 4, portalHost, ...props }, ref) => (
+const TooltipContent = React.forwardRef<TooltipPrimitive.ContentRef, TooltipPrimitive.ContentProps & { portalHost?: string }>(({ className, sideOffset = 4, portalHost, ...props }, ref) => (
   <TooltipPrimitive.Portal hostName={portalHost}>
     <TooltipPrimitive.Overlay style={Platform.OS !== 'web' ? StyleSheet.absoluteFill : undefined}>
       <Animated.View
@@ -25,7 +22,7 @@ const TooltipContent = React.forwardRef<
             sideOffset={sideOffset}
             className={cn(
               'z-50 overflow-hidden rounded-md border border-border bg-popover px-3 py-1.5 shadow-md shadow-foreground/5 web:animate-in web:fade-in-0 web:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-              className
+              className,
             )}
             {...props}
           />
