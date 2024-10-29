85c2980b87f81c8e9dff390c923a9df90eda7aba
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 10:57:39 -0500

    Refactor import paths and order in NotFoundScreen component

---
+++ app-v1/src/screens/not-found/not-found.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+const NotFoundScreen = () => {
+  return (
+    <View>
+      <Text className='text-foreground'>NotFoundScreen</Text>
+    </View>
+  );
+}
+
+export default NotFoundScreen;
+
