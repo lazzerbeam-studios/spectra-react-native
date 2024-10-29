acb199870370e389e64780e2c9642a6f5a4ffd09
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 10:58:15 -0500

    Refactor import paths and order in NotFoundScreen component, and add link to home screen

--- app-v1/src/screens/not-found/not-found.tsx
+++ app-v1/src/screens/not-found/not-found.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 const NotFoundScreen = () => {
   return (
     <View>
       <Text className='text-foreground'>NotFoundScreen</Text>
+      <Text>This screen doesn't exist.</Text>
+      <Link href='/'>
+        <Text>Go to home screen!</Text>
+      </Link>
     </View>
   );
 }
