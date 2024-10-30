a7d5f79e39ff22b3e08a9c6e8eef862c1d0436a7
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 21:43:44 -0500

    Refactor SignUpScreen component: add email input field and update import paths

--- app-v1/src/screens/sign-up/sign-up.tsx
+++ app-v1/src/screens/sign-up/sign-up.tsx
@@ -9,11 +9,19 @@ const SignUpScreen = () => {
   return (
     <SafeAreaView className='flex flex-1 flex-col items-center justify-center'>
 
-      <Text className='mb-24 text-4xl font-bold'>
-        Sign Up
-      </Text>
+      <View className='flex w-full flex-row'>
+        <View className='native:hidden flex-[0.2]'></View>
+        <View className='native:flex-1 flex-[0.6] items-center justify-center'>
 
-      <Input placeholder='Email' keyboardType='email-address' className='mb-4 w-80'></Input>
+          <Text className='mb-24 text-4xl font-bold'>
+            Sign Up
+          </Text>
+
+          <Input placeholder='Email' keyboardType='email-address' className='mb-4 w-80'></Input>
+
+        </View>
+        <View className='native:hidden flex-[0.2]'></View>
+      </View>
 
     </SafeAreaView>
   )
