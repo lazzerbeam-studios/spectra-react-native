def93c3d15c8fb865d691cd5a229126af6a3fbdc
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 23:16:55 -0500

    Refactor themeConstants.ts: update border colors in NAV_THEME

--- app-v1/src/lib/themeConstants.ts
+++ app-v1/src/lib/themeConstants.ts
@@ -1,7 +1,7 @@
 export const NAV_THEME = {
   light: {
     background: 'hsl(0 0% 100%)', // background
-    border: 'hsl(240 5.9% 90%)', // border
+    border: 'hsl(0 0% 100%)', // border
     card: 'hsl(0 0% 100%)', // card
     notification: 'hsl(0 84.2% 60.2%)', // destructive
     primary: 'hsl(240 5.9% 10%)', // primary
@@ -9,7 +9,7 @@ export const NAV_THEME = {
   },
   dark: {
     background: 'hsl(240 10% 3.9%)', // background
-    border: 'hsl(240 3.7% 15.9%)', // border
+    border: 'hsl(240 10% 3.9%)', // border
     card: 'hsl(240 10% 3.9%)', // card
     notification: 'hsl(0 72% 51%)', // destructive
     primary: 'hsl(0 0% 98%)', // primary
