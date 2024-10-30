78b6d04792b11eb06e99b89b438e6bc81d427932
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 21:58:26 -0500

    Refactor SignUpScreen component: update import paths and input styling

--- app-v1/src/screens/sign-up/sign-up.tsx
+++ app-v1/src/screens/sign-up/sign-up.tsx
@@ -13,14 +13,14 @@ const SignUpScreen = () => {
         <View className='native:hidden flex-[0.2]'></View>
         <View className='native:flex-1 flex-[0.6] items-center justify-center'>
 
-          <Text className='mb-8 text-4xl font-bold'>
+          <Text className='native:text-5xl mb-8 text-4xl font-bold'>
             Sign Up
           </Text>
 
           <Input
             placeholder='Email'
             keyboardType='email-address'
-            className='mb-4 w-80 border-4 border-foreground'
+            className='rounded-full border-2 border-foreground p-6 text-4xl'
           />
 
         </View>
