25224bdeb9a727949c4d285ce7d4324e42472fda
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 21:31:02 -0500

    Refactor SignUpScreen component: update import paths and rearrange class names and text styling

--- app-v1/src/screens/sign-up/sign-up.tsx
+++ app-v1/src/screens/sign-up/sign-up.tsx
@@ -1,11 +1,14 @@
-import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Text } from '~/src/components/ui/text';
+import { Input } from '~/src/components/ui/input';
+import { Button } from '~/src/components/ui/button';
 
 const SignUpScreen = () => {
   return (
-    <SafeAreaView className='flex flex-1 flex-col items-center justify-center bg-background'>
+    <SafeAreaView className='flex flex-1 flex-col items-center justify-center'>
 
-      <Text className='text-text mb-32 text-4xl font-bold'>
+      <Text className='mb-24 text-4xl font-bold'>
         Sign Up
       </Text>
 
