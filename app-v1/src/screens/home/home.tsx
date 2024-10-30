c59e9964cc46083d302b36e9604f91bc024211d1
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 20:53:50 -0500

    Refactor import paths and order in NotFoundScreen component, and update Button component in NotFoundScreen

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -13,16 +13,16 @@ const HomeScreen = () => {
       </Text>
 
       <Link href='/sign-up' push asChild>
-        <Button className='rounded-full'>
-          <Text>
+        <Button className='h-16 w-64 rounded-full'>
+          <Text className='text-3xl'>
             Sign Up
           </Text>
         </Button>
       </Link>
 
       <Link href='/sign-in' push asChild>
-        <Button className='rounded-full'>
-          <Text>
+        <Button className='h-16 w-64 rounded-full'>
+          <Text className='text-3xl'>
             Sign In
           </Text>
         </Button>
