1cd7c0715fcc1964054a6c6e3f0e49ad2110933a
Miguel Vazquez <migueldv90@yahoo.com>
2024-10-29 23:43:02 -0500

    Refactor SignUpScreen component: update layout and styling

--- app-v1/src/screens/sign-up/sign-up.tsx
+++ app-v1/src/screens/sign-up/sign-up.tsx
@@ -20,7 +20,7 @@ const SignUpScreen = () => {
         </Link>
       </View>
 
-      <View className='flex w-full flex-1 flex-row'>
+      <View className='mb-44 flex w-full flex-1 flex-row'>
         <View className='native:hidden flex-[0.2]'></View>
         <View className='native:flex-1 flex-[0.6] items-center justify-center'>
 
