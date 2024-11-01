3178701194cc00ce9d4a0c7c87bd454a2cb16e5e
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-01 13:48:35 -0500

    Add Zustand store for managing user profile state

---
+++ app-v1/src/stores/auth.store.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+import { Profile } from '~/src/openapi/api';
+
+type State = {
+  profile: Profile | null;
+};
+
+type Actions = {
+  setProfile: (profile: Profile) => void;
+  clear: () => void;
+};
+
+const initialState: State = {
+  profile: null,
+};
+
+export const profileStore = create<State & Actions>()((set, get) => ({
+  ...initialState,
+  setProfile: (profile: Profile) => {
+    set({ profile: profile });
+  },
+  clear: () => {
+    set(initialState);
+  },
+}));
