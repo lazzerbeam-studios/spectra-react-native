ad27e36279d7e80a5303a114868c78fb7ea0c63f
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-02 23:48:06 -0500

    Reorder apiUrl declaration for improved readability in api.ts

--- app-v1/src/api.ts
+++ app-v1/src/api.ts
@@ -2,11 +2,9 @@ import axios from 'axios';
 
 import { AuthApi, UsersApi, Configuration } from './openapi';
 
-const apiUrl = process.env.EXPO_PUBLIC_API_URL;
-
 const config = new Configuration();
-
 const axiosInstance = axios.create();
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 const authApi = new AuthApi(config, apiUrl, axiosInstance);
 const usersApi = new UsersApi(config, apiUrl, axiosInstance);
