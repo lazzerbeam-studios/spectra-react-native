1ed2683675770df0c2340aa8e69ac8ac0277007c
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-04 08:45:40 -0600

    Add ProfileUpdateScreen component with basic structure

---
+++ app-v1/src/screens/profile-update/profile-update.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ProfileUpdateScreen = () => {
+  return (
+    <View>
+      <Text>ProfileUpdateScreen</Text>
+    </View>
+  )
+}
+
+export default ProfileUpdateScreen
+
+const styles = StyleSheet.create({})
