57ecc4bd99da7f623406a030d7ac8ea1462b5d02
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-03 23:32:08 -0600

    Add profile navigation on layout submission in HomeScreen component

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,12 +1,28 @@
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Text } from '~/src/components/ui/text';
 import { Button } from '~/src/components/ui/button';
 
+import { profileStore } from '~/src/stores/profile.store';
+
 const HomeScreen = () => {
+
+  const submit = () => {
+
+    const profile = profileStore.getState().profile;
+
+    console.log(profile);
+
+    if (profile && profile.id) {
+      console.log('Here');
+
+      router.navigate('../logged-in/profile');
+    }
+  };
+
   return (
-    <SafeAreaView className='mb-36 flex flex-1 flex-col items-center justify-center'>
+    <SafeAreaView className='mb-36 flex flex-1 flex-col items-center justify-center' onLayout={submit}>
 
       <Text className='mb-24 text-8xl font-bold'>
         Spectra
