0dd5d0472d6903bce96589011c0e20eb2940d7df
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-04 12:21:12 -0600

    Add email input field to ProfileUpdateScreen with validation

--- app-v1/src/screens/profile-update/profile-update.tsx
+++ app-v1/src/screens/profile-update/profile-update.tsx
@@ -1,13 +1,21 @@
 import { View } from 'react-native';
 import { router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useForm, Controller, useFormState } from 'react-hook-form';
 
 import { Text } from '~/src/components/ui/text';
+import { Input } from '~/src/components/ui/input';
 import { Button } from '~/src/components/ui/button';
 import { ChevronLeft } from '~/src/lib/icons/Chevron';
+import { profileStore } from '~/src/stores/profile.store';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/src/components/ui/card';
 
 const ProfileUpdateScreen = () => {
+  const { profile } = profileStore();
+
+  const { control, handleSubmit } = useForm();
+  const { errors } = useFormState({ control });
+
   return (
     <SafeAreaView className='flex h-full bg-secondary/30'>
 
@@ -25,6 +33,26 @@ const ProfileUpdateScreen = () => {
 
           <Card className='w-full max-w-sm rounded-2xl p-6'>
             <CardTitle className='pb-2 text-center'>Profile Update</CardTitle>
+            <CardTitle className='pb-2 text-center'>{profile?.email}</CardTitle>
+
+            <Controller
+              name='email'
+              defaultValue={''}
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  placeholder='Email'
+                  keyboardType='email-address'
+                  className={'native:h-18 mb-6 h-16 rounded-full border-2 px-6 py-4 text-2xl'}
+                />
+              )}
+              rules={{
+                required: true,
+                pattern: /^\S+@\S+$/i,
+              }}
+            ></Controller>
+
           </Card>
 
         </View>
