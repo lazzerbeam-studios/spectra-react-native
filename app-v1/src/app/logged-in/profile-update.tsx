8038b7f1926127679d6b49b3c7c18f23cdb616da
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-04 08:48:50 -0600

    Add ProfileUpdate component to render ProfileUpdateScreen

---
+++ app-v1/src/app/logged-in/profile-update.tsx
@@ -0,0 +1,9 @@
+import ProfileUpdateScreen from '~/src/screens/profile-update/profile-update';
+
+const ProfileUpdate = () => {
+  return (
+    <ProfileUpdateScreen></ProfileUpdateScreen>
+  );
+};
+
+export default ProfileUpdate;
