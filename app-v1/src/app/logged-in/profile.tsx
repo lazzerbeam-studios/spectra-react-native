441d8633271b585a6c4d8567a1e95797ba9478dd
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-04 13:11:37 -0600

    Add Stack.Screen to Profile component for header configuration

--- app-v1/src/app/logged-in/profile.tsx
+++ app-v1/src/app/logged-in/profile.tsx
@@ -1,8 +1,11 @@
+import { Stack } from 'expo-router';
+
 import ProfileScreen from '~/src/screens/profile/profile';
 
 const Profile = () => {
   return (
     <>
+      <Stack.Screen options={{ headerShown: false }}></Stack.Screen>
       <ProfileScreen></ProfileScreen>
     </>
   );
