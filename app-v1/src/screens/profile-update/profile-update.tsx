f000ef707c4f33ec212f97cacea8f427e5f0c0d3
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-04 12:09:13 -0600

    Add navigation link with back button to ProfileUpdateScreen

--- app-v1/src/screens/profile-update/profile-update.tsx
+++ app-v1/src/screens/profile-update/profile-update.tsx
@@ -1,11 +1,25 @@
+import { Link } from 'expo-router';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Text } from '~/src/components/ui/text';
+import { Button } from '~/src/components/ui/button';
+import { ChevronLeft } from '~/src/lib/icons/Chevron';
 
 const ProfileUpdateScreen = () => {
   return (
-    <SafeAreaView className='flex h-full'>
+    <SafeAreaView className='flex h-full bg-secondary/30'>
+
+      <View className='ms-2 mt-2'>
+        <Link href='/' asChild>
+          <Button variant={'link'} size={'icon'}>
+            <ChevronLeft className='color-foreground' size={50} strokeWidth={2}></ChevronLeft>
+          </Button>
+        </Link>
+      </View>
+
       <Text>ProfileUpdateScreen</Text>
+
     </SafeAreaView>
   );
 }
