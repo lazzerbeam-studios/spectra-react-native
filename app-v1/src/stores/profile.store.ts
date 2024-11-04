7f8b29d233c787c8228cc4c327537b48d732f600
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-04 12:38:42 -0600

    Refactor profileUpdate action to accept profile parameter and update user profile data

--- app-v1/src/stores/profile.store.ts
+++ app-v1/src/stores/profile.store.ts
@@ -15,7 +15,7 @@ type Actions = {
   profileSet: (profile: Profile) => void;
   profileClear: () => void;
   profileInit: () => Promise<void>;
-  profileUpdate: () => Promise<void>;
+  profileUpdate: (profile: Profile) => Promise<void>;
 };
 
 const initState: State = {
@@ -42,16 +42,9 @@ export const profileStore = create<State & Actions>()((set, get) => ({
       get().profileClear();
     }
   },
-  profileUpdate: async () => {
-    let token = storage.getString('token');
-    if (token) {
-      authStore.getState().authSet(token);
-      token = authStore.getState().authToken;
-      const response = await usersApi.profileGet(token);
-      get().profileSet(response.data.object);
-    } else {
-      authStore.getState().authClear();
-      get().profileClear();
-    }
+  profileUpdate: async (profile: Profile) => {
+    const token = authStore.getState().authToken;
+    const response = await usersApi.profileUpdate({ object: profile }, token);
+    get().profileSet(response.data.object);
   }
 }));
