97cc882a427a6f7d8b56016d33ea4e251f887d69
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 13:23:36 -0600

    Implement LogoutDialog component with confirmation actions in profile screen

--- app-v1/src/screens/profile/alert-dialog.tsx
+++ app-v1/src/screens/profile/alert-dialog.tsx
@@ -1,11 +1,45 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '~/src/components/ui/alert-dialog';
+import { Button } from '~/src/components/ui/button';
+import { Text } from '~/src/components/ui/text';
 
-const LogoutDialog = () => {
+export const LogoutDialog = () => {
   return (
-    <View>
-      <Text>AlertDialog</Text>
+    <View className='flex-1 items-center justify-center'>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant='outline'>
+            <Text>Show Alert Dialog</Text>
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>
+              <Text>Cancel</Text>
+            </AlertDialogCancel>
+            <AlertDialogAction>
+              <Text>Continue</Text>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </View>
   );
 }
-
-export default LogoutDialog;
