42b61f49b632813143e31b68d44ab5a8b38df118
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 13:21:28 -0600

    Rename AlertDialog component to LogoutDialog in profile screen

--- app-v1/src/screens/profile/alert-dialog.tsx
+++ app-v1/src/screens/profile/alert-dialog.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native';
 
-const AlertDialog = () => {
+const LogoutDialog = () => {
   return (
     <View>
       <Text>AlertDialog</Text>
@@ -8,4 +8,4 @@ const AlertDialog = () => {
   );
 }
 
-export default AlertDialog;
+export default LogoutDialog;
