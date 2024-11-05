48cbe7ea3546bc7eaa99d7676879a9d4650f8fb3
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 00:22:36 -0600

    Add ThemeButton component with dark/light mode icons and button functionality

--- app-v1/src/screens/profile/theme-button.tsx
+++ app-v1/src/screens/profile/theme-button.tsx
@@ -1,11 +1,23 @@
-import { View } from 'react-native';
-
-import { Text } from '~/src/components/ui/text';
+import { Sun } from '~/src/lib/icons/Sun';
+import { Button } from '~/src/components/ui/button';
+import { MoonStar } from '~/src/lib/icons/MoonStar';
+import { useColorScheme } from '~/src/lib/useColorScheme';
 
 export const ThemeButton = () => {
+  const { isDarkColorScheme, setColorScheme } = useColorScheme();
+
+  const goBack = () => {
+  };
+
   return (
-    <View>
-      <Text>themeButton</Text>
-    </View>
+    <Button variant={'link'} size={'icon'} onPress={goBack}>
+
+      {isDarkColorScheme ? (
+        <MoonStar className='text-foreground' size={23} strokeWidth={1.25} />
+      ) : (
+        <Sun className='text-foreground' size={24} strokeWidth={1.25} />
+      )}
+
+    </Button>
   );
 }
