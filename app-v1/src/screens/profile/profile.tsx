34188e16f7f66c940304f5b53cb776c2faeb1408
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 00:17:38 -0600

    Add ThemeButton component and integrate into ProfileScreen header

--- app-v1/src/screens/profile/profile.tsx
+++ app-v1/src/screens/profile/profile.tsx
@@ -2,8 +2,6 @@ import { View } from 'react-native';
 import { Link, Stack } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { BackButton } from '~/src/components/back-button';
-
 import { Text } from '~/src/components/ui/text';
 import { Button } from '~/src/components/ui/button';
 import { ChevronLeft } from '~/src/lib/icons/Chevron';
@@ -11,7 +9,10 @@ import { profileStore } from '~/src/stores/profile.store';
 import { Avatar, AvatarFallback, AvatarImage } from '~/src/components/ui/avatar';
 import { Card, CardContent, CardHeader, CardTitle } from '~/src/components/ui/card';
 
+import { BackButton } from '~/src/components/back-button';
+
 import { ThemeToggle } from './ThemeToggle';
+import { ThemeButton } from './theme-button';
 
 const avatarUri = 'https://t3.ftcdn.net/jpg/07/24/59/76/360_F_724597608_pmo5BsVumFcFyHJKlASG2Y2KpkkfiYUU.jpg';
 
@@ -20,7 +21,7 @@ const ProfileScreen = () => {
 
   return (
     <>
-      <Stack.Screen options={{ title: 'Update Profile', headerTitleAlign: 'center', headerLeft: () => <BackButton></BackButton>, headerRight: () => <ThemeToggle></ThemeToggle> }} />
+      <Stack.Screen options={{ title: 'Update Profile', headerTitleAlign: 'center', headerLeft: () => <BackButton></BackButton>, headerRight: () => <ThemeButton></ThemeButton> }} />
       <SafeAreaView className='flex h-full bg-secondary/30'>
 
         <View className='mb-44 flex w-full flex-1 flex-row'>
---
+++ app-v1/src/screens/profile/theme-button.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import { Text } from '~/src/components/ui/text';
+
+export const ThemeButton = () => {
+  return (
+    <View>
+      <Text>themeButton</Text>
+    </View>
+  );
+}
