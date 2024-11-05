05cd8e0683e5c90093e52eec5dff0cb7be53e843
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 00:05:49 -0600

    Add onPress handler to BackButton for navigation functionality

--- app-v1/src/components/back-button.tsx
+++ app-v1/src/components/back-button.tsx
@@ -1,9 +1,16 @@
+import { Link, router } from 'expo-router';
+
 import { Button } from '~/src/components/ui/button';
 import { ChevronLeft } from '~/src/lib/icons/Chevron';
 
 export const BackButton = () => {
+
+  const submit = () => {
+    router.back();
+  };
+
   return (
-    <Button variant={'link'} size={'icon'}>
+    <Button variant={'link'} size={'icon'} onPress={submit}>
       <ChevronLeft className='color-foreground' size={35} strokeWidth={2}></ChevronLeft>
     </Button>
   );
