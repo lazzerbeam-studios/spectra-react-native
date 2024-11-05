7ace195ed56ea3ab5cee565f081dc036492b802d
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 15:43:09 -0600

    Add redirect for unauthenticated users in ProfileUpdate component

--- app-v1/src/app/logged-in/profile-update.tsx
+++ app-v1/src/app/logged-in/profile-update.tsx
@@ -1,8 +1,14 @@
-import { Stack } from 'expo-router';
+import { Stack, Redirect } from 'expo-router';
 
+import { AuthStore } from '~/src/stores/auth.store';
 import ProfileUpdateScreen from '~/src/screens/profile-update/profile-update';
 
 const ProfileUpdate = () => {
+  const { authBool } = AuthStore();
+  if (!authBool) {
+    return <Redirect href='/'></Redirect>;
+  }
+
   return (
     <>
       <Stack.Screen options={{ headerShown: false }}></Stack.Screen>
