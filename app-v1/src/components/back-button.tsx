156dbcf39d7bcbabee050d9541dbd9ce827dcdf7
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 00:00:39 -0600

    Refactor BackButton component: remove unnecessary View wrapper for cleaner code

--- app-v1/src/components/back-button.tsx
+++ app-v1/src/components/back-button.tsx
@@ -1,14 +1,10 @@
-import { View } from 'react-native';
-
 import { Button } from '~/src/components/ui/button';
 import { ChevronLeft } from '~/src/lib/icons/Chevron';
 
 export const BackButton = () => {
   return (
-    <View className='ps-3 pt-2'>
-      <Button variant={'link'} size={'icon'}>
-        <ChevronLeft className='color-foreground' size={50} strokeWidth={2}></ChevronLeft>
-      </Button>
-    </View>
+    <Button variant={'link'} size={'icon'}>
+      <ChevronLeft className='color-foreground' size={50} strokeWidth={2}></ChevronLeft>
+    </Button>
   );
 }
