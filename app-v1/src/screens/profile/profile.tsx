8ca8bcf5e6fa54f43aa5271a28cc95ef3112f19b
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 13:24:35 -0600

    Refactor LogoutDialog component: rename file and update imports

--- app-v1/src/screens/profile/profile.tsx
+++ app-v1/src/screens/profile/profile.tsx
@@ -9,7 +9,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '~/src/components/ui/avatar'
 import { Card, CardContent, CardHeader, CardTitle } from '~/src/components/ui/card';
 
 import { ThemeButton } from './theme-button';
-import { LogoutDialog } from './alert-dialog';
+import { LogoutDialog } from './logout-dialog';
 
 const avatarUri = 'https://t3.ftcdn.net/jpg/07/24/59/76/360_F_724597608_pmo5BsVumFcFyHJKlASG2Y2KpkkfiYUU.jpg';
 
