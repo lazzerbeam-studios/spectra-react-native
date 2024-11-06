700c8a5367b74c993a7043a098d6e548aab553fa
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-05 23:39:08 -0600

    Refactor ThemeButton component: rename file and move implementation to profile-theme-button.tsx

--- app-v1/src/screens/profile/profile.tsx
+++ app-v1/src/screens/profile/profile.tsx
@@ -8,7 +8,7 @@ import { ProfileStore } from '~/src/stores/profile.store';
 import { Avatar, AvatarFallback, AvatarImage } from '~/src/components/ui/avatar';
 import { Card, CardContent, CardHeader, CardTitle } from '~/src/components/ui/card';
 
-import { ThemeButton } from './theme-button';
+import { ThemeButton } from './profile-theme-button';
 import { LogoutDialog } from './logout-dialog';
 
 const avatarUri = 'https://t3.ftcdn.net/jpg/07/24/59/76/360_F_724597608_pmo5BsVumFcFyHJKlASG2Y2KpkkfiYUU.jpg';
