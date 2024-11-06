3f2545e90eff629e7d02ba0a2f3d3c82c45a1b90
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 11:05:35 -0600

    Add Stack.Screen configuration for DashboadScreen with custom header options

--- app-v1/src/screens/dashboad/dashboad.tsx
+++ app-v1/src/screens/dashboad/dashboad.tsx
@@ -1,10 +1,20 @@
 import { View } from 'react-native';
+import { Stack } from 'expo-router';
 
 import { Text } from '~/src/components/ui/text';
 
 export const DashboadScreen = () => {
   return (
     <>
+
+      <Stack.Screen
+        options={{
+          title: '',
+          headerTitleAlign: 'center',
+          headerShadowVisible: false,
+          headerLeft: () => <></>,
+        }}>
+      </Stack.Screen>
 
       <View>
         <Text>dashboad</Text>
