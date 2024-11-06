41cf7d0aad2dbdc52a25fcb35b9d867c31478c62
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 00:51:43 -0600

    Refactor Dashboard component to import Text from UI components

--- app-v1/src/screens/dashboad/dashboad.tsx
+++ app-v1/src/screens/dashboad/dashboad.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import { Text } from '~/src/components/ui/text';
 
 export default function dashboad() {
   return (
