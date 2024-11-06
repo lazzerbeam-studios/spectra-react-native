5b3564b96abc34b185094f021f12c8e596f034f2
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 12:22:01 -0600

    Remove unnecessary whitespace in AppLayout component

--- app-v1/src/app/(app)/_layout.tsx
+++ app-v1/src/app/(app)/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack, Redirect } from 'expo-router';
 import { AuthStore } from '~/src/stores/auth.store';
 
 const AppLayout = () => {
-
   const { authBool } = AuthStore();
   if (!authBool) {
     return <Redirect href='/'></Redirect>;
