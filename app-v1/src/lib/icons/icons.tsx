076e2bf8bf4a6976f9b030c546264c1cd49e1088
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 12:56:50 -0600

    Remove duplicate Sun icon registration in iconWithClassName function

--- app-v1/src/lib/icons/icons.tsx
+++ app-v1/src/lib/icons/icons.tsx
@@ -2,9 +2,9 @@ import { ChevronLeft, CircleUserRound, MoonStar, Sun } from 'lucide-react-native
 
 import { iconWithClassName } from './iconWithClassName';
 
+iconWithClassName(Sun);
 iconWithClassName(MoonStar);
 iconWithClassName(ChevronLeft);
 iconWithClassName(CircleUserRound);
-iconWithClassName(Sun);
 
 export { ChevronLeft, CircleUserRound, MoonStar, Sun };
