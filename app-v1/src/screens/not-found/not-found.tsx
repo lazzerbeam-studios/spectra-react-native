31586c466929595c15c6e75de4ea27a8704f2c6b
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 00:06:22 -0600

    Refactor Profile and NotFoundScreen components: update Profile to use named export and improve JSX structure in NotFoundScreen

--- app-v1/src/screens/not-found/not-found.tsx
+++ app-v1/src/screens/not-found/not-found.tsx
@@ -6,20 +6,22 @@ import { Button } from '~/src/components/ui/button';
 
 export const NotFoundScreen = () => {
   return (
-    <View className='flex-1 items-center justify-center'>
+    <>
+      <View className='flex-1 items-center justify-center'>
 
-      <Text className='text-5xl'>
-        404 Not Found
-      </Text>
+        <Text className='text-5xl'>
+          404 Not Found
+        </Text>
 
-      <Link href='/' asChild>
-        <Button className='native:h-20 mb-4 mt-8 h-16 rounded-full pe-8 ps-8'>
-          <Text className='native:text-2xl text-2xl'>
-            Go Home
-          </Text>
-        </Button>
-      </Link>
+        <Link href='/' asChild>
+          <Button className='native:h-20 mb-4 mt-8 h-16 rounded-full pe-8 ps-8'>
+            <Text className='native:text-2xl text-2xl'>
+              Go Home
+            </Text>
+          </Button>
+        </Link>
 
-    </View>
+      </View>
+    </>
   );
 }
