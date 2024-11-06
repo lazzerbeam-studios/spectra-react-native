b1f4890963cadbdfa4cc6b3e730b2fc6239fa99a
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 11:07:02 -0600

    Update DashboadScreen layout with responsive design and improved structure

--- app-v1/src/screens/dashboad/dashboad.tsx
+++ app-v1/src/screens/dashboad/dashboad.tsx
@@ -16,8 +16,14 @@ export const DashboadScreen = () => {
         }}>
       </Stack.Screen>
 
-      <View>
-        <Text>dashboad</Text>
+      <View className='flex h-full w-full flex-1 flex-row bg-secondary/30'>
+        <View className='hidden flex-[0.2] sm:flex'></View>
+        <View className='flex-1 items-center sm:flex-[0.6]'>
+
+          <Text>Dashboad</Text>
+
+        </View>
+        <View className='hidden flex-[0.2] sm:flex'></View>
       </View>
 
     </>
