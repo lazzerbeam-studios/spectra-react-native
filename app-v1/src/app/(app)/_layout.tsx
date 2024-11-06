d6dc7b1c025a4b445adaa1b256e473314d6caa47
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 12:20:33 -0600

    Implement authentication check in AppLayout and remove redundant redirect from Profile component

--- app-v1/src/app/(app)/_layout.tsx
+++ app-v1/src/app/(app)/_layout.tsx
@@ -1,6 +1,14 @@
-import { Stack } from 'expo-router';
+import { Stack, Redirect } from 'expo-router';
+
+import { AuthStore } from '~/src/stores/auth.store';
 
 const AppLayout = () => {
+
+  const { authBool } = AuthStore();
+  if (!authBool) {
+    return <Redirect href='/'></Redirect>;
+  }
+
   return (
     <Stack></Stack>
   );
--- app-v1/src/app/(app)/profile.tsx
+++ app-v1/src/app/(app)/profile.tsx
@@ -1,14 +1,6 @@
-import { Redirect } from 'expo-router';
-
-import { AuthStore } from '~/src/stores/auth.store';
 import { ProfileScreen } from '~/src/screens/profile/profile';
 
 const Profile = () => {
-  const { authBool } = AuthStore();
-  if (!authBool) {
-    return <Redirect href='/'></Redirect>;
-  }
-
   return (
     <ProfileScreen></ProfileScreen>
   );
