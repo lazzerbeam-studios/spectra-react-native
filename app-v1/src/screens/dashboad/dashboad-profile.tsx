015901fd41e07ae169153876e9c2a2200865b035
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 11:17:56 -0600

    Replace DashboadProfile text with icon button for enhanced UI

--- app-v1/src/screens/dashboad/dashboad-profile.tsx
+++ app-v1/src/screens/dashboad/dashboad-profile.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native';
+import { Button } from '~/src/components/ui/button';
+import { MoonStar } from '~/src/lib/icons/MoonStar';
 
 export const DashboadProfile = () => {
   return (
-    <View>
-      <Text>DashboadProfile</Text>
-    </View>
+    <Button className='web:me-2' variant={'link'} size={'icon'}>
+      <MoonStar className='text-foreground' size={30} strokeWidth={1.25}></MoonStar>
+    </Button>
   );
 }
