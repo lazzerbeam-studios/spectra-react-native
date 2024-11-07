36f67ff8b1b58f0d55a63481aa29ec5e241387e0
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 22:16:38 -0600

    Update padding for Spectra text in HomeScreen component

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -17,7 +17,7 @@ export const HomeScreen = () => {
   return (
     <SafeAreaView className='mb-36 flex flex-1 flex-col items-center justify-center' onLayout={onLayout}>
 
-      <Text className='mb-24 font-Poppins600 text-8xl'>
+      <Text className='native:pt-2 mb-24 font-Poppins600 text-8xl'>
         Spectra
       </Text>
 
