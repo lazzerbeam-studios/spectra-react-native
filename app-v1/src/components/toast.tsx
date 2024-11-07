3aeb73877aaec71e38fc8c9eba84b3d5971b29fb
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-07 10:20:00 -0600

    Add CircleAlert icon to error toast for enhanced alert representation

--- app-v1/src/components/toast.tsx
+++ app-v1/src/components/toast.tsx
@@ -1,12 +1,17 @@
 import { View } from 'react-native';
 
 import { Text } from '~/src/components/ui/text';
+import { CircleAlert } from '~/src/icons/icons';
 
 export const ToastConfig = {
   error: (props: any) => (
     <View className='w-full max-w-sm rounded-md border-2 border-red-500 bg-background px-6 py-4'>
+
+      <CircleAlert className='text-red-500'></CircleAlert>
+
       <Text className='native:text-xl mb-2 text-xl font-bold'>{props.text1}</Text>
       <Text className='native:text-base text-lg'>{props.text2}</Text>
+
     </View>
   ),
 };
