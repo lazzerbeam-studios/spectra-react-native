edca7d2a1a3b30498a34835b0ded5632f4750858
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 21:57:33 -0600

    Add Poppins bold variant to Tailwind CSS configuration

--- app-v1/tailwind.config.js
+++ app-v1/tailwind.config.js
@@ -69,11 +69,10 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
-
       fontFamily: {
         Poppins400: ["Poppins400"],
+        Poppins700: ["Poppins700"],
       },
-
     },
   },
 };
