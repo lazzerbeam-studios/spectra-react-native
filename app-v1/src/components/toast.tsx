80a2c52d55b76f65bf44d9f7a18bf75bde2628b3
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-07 10:40:58 -0600

    Refactor ToastConfig to simplify the error toast structure for improved readability

--- app-v1/src/components/toast.tsx
+++ app-v1/src/components/toast.tsx
@@ -6,23 +6,15 @@ import { CircleAlert } from '~/src/icons/icons';
 export const ToastConfig = {
   error: ({ text1, text2, props }: { text1?: string; text2?: string; props: any }) => (
     <View className='w-full max-w-sm rounded-md border-2 border-red-500 bg-background p-4'>
-
       <View className='flex-row'>
-
         <View className='me-3 mt-0.5'>
-
           <CircleAlert className='text-red-500'></CircleAlert>
-
         </View>
         <View className='flex-1'>
-
           <Text className='native:text-xl mb-1 text-xl font-bold'>{text1}</Text>
           <Text className='native:text-base text-lg'>{text2}</Text>
-
         </View>
       </View>
-
     </View>
-
   ),
 };
