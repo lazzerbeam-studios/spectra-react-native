6afa8f931cbf81a95ad1a97fc33c26f58f418252
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-06 22:53:37 -0600

    Enhance errorGet function to handle array of errors and improve error message formatting

--- app-v1/src/scripts/errors.ts
+++ app-v1/src/scripts/errors.ts
@@ -6,5 +6,16 @@ export const errorGet = (error: any) => {
     errorString = error.detail
   }
 
+  if (error.length >= 1) {
+    errorString = '';
+    errorString = (error[0].location) ? errorString.concat('', error[0].location) : errorString;
+    errorString = (error[0].location && error[0].message) ? errorString.concat('', ': ') : errorString;
+    errorString = (error[0].message) ? errorString.concat('', error[0].message) : errorString;
+  }
+
+  if (error.errors && error.errors.length >= 1) {
+    errorString = error.errors[0].message;
+  }
+
   return errorString;
 }
