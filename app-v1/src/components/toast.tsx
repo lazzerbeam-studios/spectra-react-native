4fa650ca163cfb165374ef3091d9dab7e7081dd6
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-07 10:08:44 -0600

    Refactor tomatoToast component styling for improved layout and appearance

--- app-v1/src/components/toast.tsx
+++ app-v1/src/components/toast.tsx
@@ -6,11 +6,9 @@ import { Text } from '~/src/components/ui/text';
 
 export const toastConfig = {
   tomatoToast: (props: any) => (
-    <View>
-
+    <View className='w-full max-w-sm rounded-md border-2 border-red-500 px-6'>
       <Text>{props.text1}</Text>
       <Text>{props.text2}</Text>
-
     </View>
   ),
 };
