0d8e84ecb869682b9eed294e6048d6200f558271
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-07 10:24:07 -0600

    Refactor ToastConfig to improve layout structure and enhance text styling in error notifications

--- app-v1/src/components/toast.tsx
+++ app-v1/src/components/toast.tsx
@@ -8,12 +8,13 @@ export const ToastConfig = {
     <View className='w-full max-w-sm rounded-md border-2 border-red-500 bg-background px-6 py-4'>
 
       <View className='flex-row'>
+
         <View className=''>
 
           <CircleAlert className='text-red-500'></CircleAlert>
 
         </View>
-        <View className=''>
+        <View className='flex-1'>
 
           <Text className='native:text-xl mb-2 text-xl font-bold'>{props.text1}</Text>
           <Text className='native:text-base text-lg'>{props.text2}</Text>
