0a6400714796d0226a1107df55e5bfff7f2f3caa
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-07 10:11:11 -0600

    Refactor ToastConfig to add padding for improved layout in error notifications

--- app-v1/src/components/toast.tsx
+++ app-v1/src/components/toast.tsx
@@ -4,7 +4,7 @@ import { Text } from '~/src/components/ui/text';
 
 export const ToastConfig = {
   error: (props: any) => (
-    <View className='w-full max-w-sm rounded-md border-2 border-red-500 px-6'>
+    <View className='w-full max-w-sm rounded-md border-2 border-red-500 px-6 py-4'>
       <Text>{props.text1}</Text>
       <Text>{props.text2}</Text>
     </View>
