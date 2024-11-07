1618f63777b28502a83549c54f231fc43759d41a
Miguel Vazquez <migueldv90@yahoo.com>
2024-11-07 10:14:16 -0600

    Refactor ToastConfig to enhance text styling for improved readability in error notifications

--- app-v1/src/components/toast.tsx
+++ app-v1/src/components/toast.tsx
@@ -5,7 +5,7 @@ import { Text } from '~/src/components/ui/text';
 export const ToastConfig = {
   error: (props: any) => (
     <View className='w-full max-w-sm rounded-md border-2 border-red-500 bg-background px-6 py-4'>
-      <Text>{props.text1}</Text>
+      <Text className='native:text-xl mb-2 text-xl font-bold'>{props.text1}</Text>
       <Text>{props.text2}</Text>
     </View>
   ),
