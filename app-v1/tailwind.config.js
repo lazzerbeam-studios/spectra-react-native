857e91c7f3641b3efa974688956c9c5f6762830b
Miguel Vazquez <migueldv90@gmail.com>
2025-05-31 00:07:05 -0500

    Refactor tailwind.config.js for improved formatting and readability; ensure content and presets are clearly defined

--- app-v1/tailwind.config.js
+++ app-v1/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
 
-  presets: [require('nativewind/preset')],
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+  ],
+  presets: [
+    require('nativewind/preset'),
+  ],
   theme: {
     extend: {},
   },
