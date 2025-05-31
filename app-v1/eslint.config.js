9452145f3363938f1f254b7c1309ad060137ad02
Miguel Vazquez <migueldv90@gmail.com>
2025-05-31 00:39:54 -0500

    Update eslint.config.js to format ignores array for improved readability

--- app-v1/eslint.config.js
+++ app-v1/eslint.config.js
@@ -5,7 +5,9 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: [
+      'dist/*',
+    ],
   },
   {
     rules: {
