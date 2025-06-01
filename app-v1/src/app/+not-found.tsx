764fe63c5f5ddb51f7149d810108282be1812615
Miguel Vazquez <migueldv90@gmail.com>
2025-06-01 01:12:04 -0500

    Implement NotFoundScreen component with updated layout and styling; replace Button with TouchableOpacity for improved user experience in 404 error handling.

--- app-v1/src/app/+not-found.tsx
+++ app-v1/src/app/+not-found.tsx
@@ -1,9 +1,12 @@
 import { Stack } from 'expo-router';
 
+import { NotFoundScreen } from '~/src/screens/not-found/not-found';
+
 const NotFound = () => {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }}></Stack.Screen>
+      <NotFoundScreen></NotFoundScreen>
     </>
   );
 }
