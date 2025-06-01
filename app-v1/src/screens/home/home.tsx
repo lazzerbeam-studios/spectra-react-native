ed7c9dd832378e071de69009705ebe8402dd32c5
Miguel Vazquez <migueldv90@gmail.com>
2025-06-01 01:57:25 -0500

    Refactor HomeScreen component for improved styling consistency
    - Remove unnecessary 'native:' prefixes from Tailwind classes.
    - Streamline button styles for Sign Up and Sign In links.
    - Enhance overall readability and maintainability of the HomeScreen code.

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -16,21 +16,21 @@ export const HomeScreen = () => {
   return (
     <SafeAreaView className='mb-36 flex flex-1 flex-col items-center justify-center' onLayout={onLayout}>
 
-      <Text className='native:pt-2 mb-24 font-Poppins500 text-8xl'>
+      <Text className='mb-24 font-Poppins500 text-8xl'>
         Spectra
       </Text>
 
       <Link href='/sign-up' push asChild>
-        <TouchableOpacity className='native:h-20 mb-6 h-20 w-96 rounded-full'>
-          <Text className='native:text-4xl text-4xl'>
+        <TouchableOpacity className='mb-6 h-20 w-96 rounded-full'>
+          <Text className='text-4xl'>
             Sign Up
           </Text>
         </TouchableOpacity>
       </Link>
 
       <Link href='/sign-in' push asChild>
-        <TouchableOpacity className='native:h-20 h-20 w-96 rounded-full'>
-          <Text className='native:text-4xl text-4xl'>
+        <TouchableOpacity className='h-20 w-96 rounded-full'>
+          <Text className='text-4xl'>
             Sign In
           </Text>
         </TouchableOpacity>
