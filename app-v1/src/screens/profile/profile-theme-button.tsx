c56d6872117d41ff36182b7352ce31e5f1d2a945
Miguel Vazquez <migueldv90@gmail.com>
2025-06-01 22:23:16 -0500

    Refactor ProfileThemeButton for improved functionality and styling
    - Replace commented-out code with functional implementation using Ionicons for theme toggling.
    - Utilize NativeWind for consistent styling and enhance user interaction with TouchableOpacity.
    - Maintain adherence to React Native and TypeScript best practices for improved code clarity.

--- app-v1/src/screens/profile/profile-theme-button.tsx
+++ app-v1/src/screens/profile/profile-theme-button.tsx
@@ -1,28 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// import { MoonStar, Sun } from '~/src/icons/icons';
-// import { Button } from '~/src/components/ui/button';
-// // import { useColorScheme } from '~/src/lib/useColorScheme';
-// import { setAndroidNavigationBar } from '~/src/lib/setAndroidNavigationBar';
+import { useColorScheme } from 'nativewind';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 export const ProfileThemeButton = () => {
-  // const { isDarkColorScheme, setColorScheme } = useColorScheme();
+  const { colorScheme, setColorScheme } = useColorScheme();
 
-  // const changeTheme = () => {
-  //   const colorTheme = isDarkColorScheme ? 'light' : 'dark';
-  //   setColorScheme(colorTheme);
-  //   setAndroidNavigationBar(colorTheme);
-  //   AsyncStorage.setItem('colorTheme', colorTheme);
-  // };
+  const changeTheme = () => {
+    const colorTheme = (colorScheme === 'dark') ? 'light' : 'dark';
+    setColorScheme(colorTheme);
+    AsyncStorage.setItem('colorTheme', colorTheme);
+  };
 
   return (
-    <></>
-    // <Button className='web:me-2' variant={'link'} size={'icon'} onPress={changeTheme}>
-    //   {isDarkColorScheme ? (
-    //     <MoonStar className='text-foreground' size={30} strokeWidth={1.25}></MoonStar>
-    //   ) : (
-    //     <Sun className='text-foreground' size={30} strokeWidth={1.25}></Sun>
-    //   )}
-    // </Button>
+    <TouchableOpacity className='p-2 web:me-2' onPress={changeTheme}>
+      {(colorScheme === 'dark') ? (
+        <Ionicons name="moon" size={30} color="black" />
+      ) : (
+        <Ionicons name="sunny" size={30} color="black" />
+      )}
+    </TouchableOpacity>
   );
 }
