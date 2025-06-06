7cf2fcb113ce42ccf621979a746170da1c653dfb
Miguel Vazquez <migueldv90@gmail.com>
2025-06-05 22:39:29 -0500

    Remove deprecated files and configurations from the project
    - Delete unused environment files (.env, .env.stag) and configuration files (babel.config.js, tailwind.config.js, metro.config.js) to streamline the codebase.
    - Remove all screens, components, and assets related to the application to ensure a clean slate for future development.
    - Eliminate unnecessary scripts and store files to enhance maintainability and clarity.

--- app-v2/src/app/(app)/profile-update.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileUpdateScreen } from '~/src/screens/profile-update/profile-update';
-
-const ProfileUpdate = () => {
-  return (
-    <ProfileUpdateScreen></ProfileUpdateScreen>
-  );
-};
-
-export default ProfileUpdate;
--- app-v2/src/app/(app)/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileScreen } from '~/src/screens/profile/profile';
-
-const Profile = () => {
-  return (
-    <ProfileScreen></ProfileScreen>
-  );
-};
-
-export default Profile;
