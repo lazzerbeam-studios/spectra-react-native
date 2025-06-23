2ec4170e8163216bce96a9d4a293b4048adac438
Miguel Vazquez <migueldv90@gmail.com>
2025-06-22 19:46:27 -0500

    Add error logging in ProfileUpdateScreen component

--- app-v1/src/screens/profile-update/profile-update.tsx
+++ app-v1/src/screens/profile-update/profile-update.tsx
@@ -20,6 +20,7 @@ export const ProfileUpdateScreen = () => {
           router.back();
         }
       } catch (error: any) {
+        console.log(error);
       }
     }
   };
@@ -81,4 +82,4 @@ export const ProfileUpdateScreen = () => {
 
     </>
   );
-}
+};
