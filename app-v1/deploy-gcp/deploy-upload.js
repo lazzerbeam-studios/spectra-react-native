e0cf66c0175238c5d0165d128ac32910a6db0315
Miguel Vazquez <migueldv90@gmail.com>
2025-09-03 23:28:28 -0500

    Refactor OpenAPI generator script for improved organization
    - Updated the OpenAPI generator script to reflect new file paths and output directory.
    - Ensured generated files are correctly organized within the project structure.
    - Maintained consistency with recent changes to the codebase.

---
+++ app-v1/deploy-gcp/deploy-upload.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+
+const { bucket, folderPath } = require('./deploy-env');
+
+const fileStorageUpload = async (fileStream, filePathStorage) => {
+  try {
+    const fileStorage = bucket.file(filePathStorage);
+
+    let metadata = {
+      contentEncoding: 'gzip',
+      cacheControl: 'max-age=3600, public',
+    };
+    if (filePathStorage === 'index.html') {
+      metadata = {
+        contentEncoding: 'gzip',
+        cacheControl: 'max-age=30, public',
+      };
+    }
+
+    const writeStream = fileStorage.createWriteStream({
+      metadata: metadata,
+    });
+    fileStream.pipe(writeStream);
+
+    return new Promise((resolve, reject) => {
+      writeStream.on('finish', async () => {
+        try {
+          const [fileMetadata] = await fileStorage.getMetadata();
+          resolve(fileMetadata.mediaLink);
+        }
+        catch (err) {
+          reject(err);
+        }
+      });
+      writeStream.on('error', reject);
+    });
+  }
+  catch (error) {
+    console.error('Error during upload:', error);
+    throw new Error(error.message);
+  }
+};
+
+const fileGzipUpload = async (filePathLocal, filePathStorage) => {
+  const gzip = zlib.createGzip();
+  const fileSource = fs.createReadStream(filePathLocal);
+  const fileStream = fileSource.pipe(gzip);
+  try {
+    const mediaLink = await fileStorageUpload(fileStream, filePathStorage);
+    console.log('File uploaded successfully:', mediaLink);
+  }
+  catch (error) {
+    console.error('Error uploading file:', error);
+  }
+};
+
+const uploadFoldersFiles = async (folderPath, relativePath) => {
+  try {
+    const files = await fs.promises.readdir(folderPath);
+    for (const file of files) {
+      const filePathLocal = path.join(folderPath, file);
+      const filePathStorage = path.join(relativePath, file);
+      const fileStat = await fs.promises.stat(filePathLocal);
+      if (fileStat.isFile()) {
+        await fileGzipUpload(filePathLocal, filePathStorage);
+      } else if (fileStat.isDirectory()) {
+        await uploadFoldersFiles(filePathLocal, filePathStorage);
+      }
+    }
+  }
+  catch (error) {
+    console.error('Error reading folders and files:', error);
+  }
+};
+
+uploadFoldersFiles(folderPath, '');
