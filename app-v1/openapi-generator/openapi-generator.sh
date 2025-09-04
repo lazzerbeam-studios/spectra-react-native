530b37bfef26afd610a3db92caa5e357c57ce527
Miguel Vazquez <migueldv90@gmail.com>
2025-09-03 23:07:51 -0500

    Update OpenAPI generator script to reflect new file paths
    - Changed output path for `openapi-schema.json` to the `openapi-generator` directory.
    - Updated the OpenAPI generator command to use the new schema file location.
    - This change ensures proper organization of generated files within the project structure.

--- app-v1/openapi-generator/openapi-generator.sh
+++ app-v1/openapi-generator/openapi-generator.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
 content=$(curl -L http://localhost:8080/openapi.json)
-echo $content > openapi-schema.json
+echo $content > openapi-generator/openapi-schema.json
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -g typescript-axios -i /local/openapi-schema.json -o /local/src/openapi
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -g typescript-axios -i /local/openapi-generator/openapi-schema.json -o /local/openapi-generator/src/openapi
