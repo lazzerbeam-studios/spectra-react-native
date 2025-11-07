c4b14793a9e4482c4ceffd06fd27ec1ee5e915d8
Miguel Vazquez <migueldv90@gmail.com>
2025-11-07 10:49:33 -0600

    Refactor HomeScreen to replace TouchableOpacity with Pressable for improved touch handling and responsiveness in navigation links.

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link, router } from 'expo-router';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ProfileStore } from '~/src/stores/profile.store';
@@ -25,19 +25,19 @@ export const HomeScreen = () => {
           </Text>
 
           <Link href='/sign-up' push asChild>
-            <TouchableOpacity className='mb-6 h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
+            <Pressable className='mb-6 h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
               <Text className='font-Poppins500 text-4xl text-background'>
                 Sign Up
               </Text>
-            </TouchableOpacity>
+            </Pressable>
           </Link>
 
           <Link href='/sign-in' push asChild>
-            <TouchableOpacity className='h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
+            <Pressable className='h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
               <Text className='font-Poppins500 text-4xl text-background'>
                 Sign In
               </Text>
-            </TouchableOpacity>
+            </Pressable>
           </Link>
 
         </View>
