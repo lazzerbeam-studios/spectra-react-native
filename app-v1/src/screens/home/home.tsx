e8f47b6bb23331dc7a2e2317b4ea4f8b181c6452
Miguel Vazquez <migueldv90@gmail.com>
2025-11-07 10:44:05 -0600

    Refactor HomeScreen layout by adding a View wrapper for improved structure and clarity, while maintaining existing functionality for navigation links.

--- app-v1/src/screens/home/home.tsx
+++ app-v1/src/screens/home/home.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link, router } from 'expo-router';
-import { Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ProfileStore } from '~/src/stores/profile.store';
@@ -15,27 +15,33 @@ export const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView className='mb-36 flex flex-1 flex-col items-center justify-center'>
+    <SafeAreaView>
 
-      <Text className='mb-24 pt-2 font-Poppins500 text-8xl text-foreground'>
-        Spectra
-      </Text>
+      <View className='items-center'>
+        <View className='w-full sm:w-1/3'>
 
-      <Link href='/sign-up' push asChild>
-        <TouchableOpacity className='mb-6 h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
-          <Text className='font-Poppins500 text-4xl text-background'>
-            Sign Up
+          <Text className='mb-24 pt-2 font-Poppins500 text-8xl text-foreground'>
+            Spectra
           </Text>
-        </TouchableOpacity>
-      </Link>
 
-      <Link href='/sign-in' push asChild>
-        <TouchableOpacity className='h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
-          <Text className='font-Poppins500 text-4xl text-background'>
-            Sign In
-          </Text>
-        </TouchableOpacity>
-      </Link>
+          <Link href='/sign-up' push asChild>
+            <TouchableOpacity className='mb-6 h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
+              <Text className='font-Poppins500 text-4xl text-background'>
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </Link>
+
+          <Link href='/sign-in' push asChild>
+            <TouchableOpacity className='h-20 w-96 items-center justify-center rounded-full bg-foreground pt-1'>
+              <Text className='font-Poppins500 text-4xl text-background'>
+                Sign In
+              </Text>
+            </TouchableOpacity>
+          </Link>
+
+        </View>
+      </View>
 
     </SafeAreaView>
   );
