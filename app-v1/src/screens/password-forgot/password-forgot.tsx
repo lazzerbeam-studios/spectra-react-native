b68a5b80b1fe49c7bb6c4812e152977e7328ee0a
Miguel Vazquez <migueldv90@gmail.com>
2025-11-10 08:23:51 -0600

    Refactor PasswordForgotScreen component by separating header and content into distinct components for improved structure and readability. Removed unused imports and simplified layout.

--- app-v1/src/screens/password-forgot/password-forgot.tsx
+++ app-v1/src/screens/password-forgot/password-forgot.tsx
@@ -1,84 +1,19 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-
-import { Link, router } from 'expo-router';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useForm, Controller, useFormState } from 'react-hook-form';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-import { authApi } from '~/src/api';
-import { ErrorGet } from '~/src/scripts/error';
+import { PasswordForgotHeader } from './password-forgot-header';
+import { PasswordForgotContent } from './password-forgot-content';
 
 export const PasswordForgotScreen = () => {
-  const { control, handleSubmit } = useForm();
-  const { errors } = useFormState({ control });
-
-  const submit = async (data: any) => {
-    try {
-      await authApi.passwordForgotPostAPI({
-        email: data.email,
-      });
-      router.replace('/password-reset');
-    } catch (errors: any) {
-      const error = ErrorGet(errors.response.data);
-      console.log(error);
-    }
-  };
-
   return (
-    <SafeAreaView className='flex h-full'>
+    <SafeAreaView>
 
-      <View className='ms-2 mt-2'>
-        <Link href='/sign-in' asChild>
-          <TouchableOpacity className='p-2'>
-            <Ionicons className="color-foreground" name="chevron-back" size={50} />
-          </TouchableOpacity>
-        </Link>
-      </View>
+      <PasswordForgotHeader />
 
-      <View className='mb-44 flex w-full flex-1 flex-row'>
-        <View className='native:hidden flex-[0.2]'></View>
-        <View className='native:flex-1 flex-[0.6] items-center justify-center'>
-
-          <Text className='mb-6 text-4xl font-bold text-foreground'>
-            Forgot Password
-          </Text>
-
-          <Controller
-            name='email'
-            defaultValue={''}
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                value={value}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                placeholder='Email'
-                keyboardType='email-address'
-                className='mb-6 h-16 w-96 rounded-full border-2 border-foreground px-6 py-4 text-2xl text-foreground'
-              ></TextInput>
-            )}
-            rules={{
-              required: true,
-              pattern: /^\S+@\S+$/i,
-            }}
-          ></Controller>
-
-          {errors.email && (
-            <Text className="-mt-4 mb-2 w-80 text-sm text-red-500">
-              Please enter a valid email address
-              {errors.email.type === 'required' && ' (Email is required)'}
-              {errors.email.type === 'pattern' && ' (Please enter a valid email format)'}
-            </Text>
-          )}
-
-          <TouchableOpacity className='mb-6 h-16 w-96 items-center justify-center rounded-full bg-foreground' onPress={handleSubmit(submit)}>
-            <Text className='text-3xl text-background'>
-              Send
-            </Text>
-          </TouchableOpacity>
-
+      <View className='items-center'>
+        <View className='w-full pt-16 sm:w-1/3'>
+          <PasswordForgotContent />
         </View>
-        <View className='native:hidden flex-[0.2]'></View>
       </View>
 
     </SafeAreaView>
