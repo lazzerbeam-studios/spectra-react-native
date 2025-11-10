c35ca1243d93e782dab4d37648a309789a83d0b6
Miguel Vazquez <migueldv90@gmail.com>
2025-11-10 09:02:02 -0600

    Add DashboardHeader component: Implement layout with navigation link and styles for improved dashboard header display.

--- app-v1/src/screens/dashboard/dashboard-header.tsx
+++ app-v1/src/screens/dashboard/dashboard-header.tsx
@@ -1,4 +1,26 @@
+import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+
+const logoWidth = 1602;
+const logoHeight = 755;
+const logoAspectRatio = logoWidth / logoHeight;
+
 export const DashboardHeader = () => {
-  return null;
+  return (
+    <View className='h-16 flex-row items-center'>
+      <View className='ms-2'>
+        <Link href='/dashboard' asChild>
+        </Link>
+      </View>
+      <View className='flex-1'></View>
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  image: {
+    height: 40,
+    marginStart: 16,
+    aspectRatio: logoAspectRatio,
+  },
+});
