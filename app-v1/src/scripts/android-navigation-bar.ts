015d5731c72fdcd2198dbed701bf64826c1794b6
Miguel Vazquez <migueldv90@gmail.com>
2025-11-09 23:10:54 -0600

    Refactor Android navigation bar settings to use NavTheme instead of THEME for background color management, ensuring consistency with updated theme structure.

--- app-v1/src/scripts/android-navigation-bar.ts
+++ app-v1/src/scripts/android-navigation-bar.ts
@@ -1,9 +1,9 @@
-import { THEME } from '~/src/theme';
+import { NavTheme } from '~/src/theme';
 import { Platform } from 'react-native';
 import * as NavigationBar from 'expo-navigation-bar';
 
 export const AndroidNavigationBarSet = async (theme: 'light' | 'dark') => {
   if (Platform.OS !== 'android') return;
   await NavigationBar.setButtonStyleAsync((theme === 'dark') ? 'light' : 'dark');
-  await NavigationBar.setBackgroundColorAsync((theme === 'dark') ? THEME.dark.background : THEME.light.background);
+  await NavigationBar.setBackgroundColorAsync((theme === 'dark') ? NavTheme.dark.background : NavTheme.light.background);
 };
