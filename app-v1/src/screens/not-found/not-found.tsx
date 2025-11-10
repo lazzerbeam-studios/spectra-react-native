91d32719107eb1cbb1b443143d054c1a470f1806
Miguel Vazquez <migueldv90@gmail.com>
2025-11-10 08:30:37 -0600

    Refactor NotFoundScreen component: Update layout and styling for improved user experience, including new text elements and a button for navigation.

--- app-v1/src/screens/not-found/not-found.tsx
+++ app-v1/src/screens/not-found/not-found.tsx
@@ -1,24 +1,38 @@
 import { Link } from 'expo-router';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Text } from '~/src/components/ui/text';
+import { Button } from '~/src/components/ui/button';
 
 export const NotFoundScreen = () => {
   return (
-    <SafeAreaView className='flex-1'>
-      <View className='flex-1 items-center justify-center'>
+    <SafeAreaView>
+      <View className='items-center'>
+        <View className='w-full items-center px-6 pt-20 sm:w-1/3'>
 
-        <Text className='text-4xl color-foreground'>
-          404 Not Found
-        </Text>
+          <Text className="mb-8 text-8xl font-black tracking-tighter">
+            404
+          </Text>
 
-        <Link href='/' asChild>
-          <TouchableOpacity className='mb-4 mt-8 rounded-full bg-foreground px-8 py-4'>
-            <Text className='text-2xl color-background'>
-              Go Home
+          <View className="mb-8">
+            <Text variant='h1' className='mb-2 text-center text-3xl'>
+              Page Not Found
             </Text>
-          </TouchableOpacity>
-        </Link>
+            <Text variant='muted' className='max-w-sm text-center text-base leading-relaxed'>
+              The page you&apos;re looking for doesn&apos;t exist or has been moved.
+            </Text>
+          </View>
 
+          <Link href='/' asChild>
+            <Button size='xl' variant='default' className='w-full'>
+              <Text>
+                Go Home
+              </Text>
+            </Button>
+          </Link>
+
+        </View>
       </View>
     </SafeAreaView>
   );
