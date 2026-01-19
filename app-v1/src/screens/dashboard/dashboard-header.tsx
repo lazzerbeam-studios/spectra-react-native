05d843080f900a68751bba0d382c85064e5be5f3
Miguel Vazquez <migueldv90@gmail.com>
2026-01-18 22:52:06 -0600

    Refactor DashboardHeader component to replace CircleUserRound icon with a dynamic Avatar that displays user profile image or fallback text. This enhances user personalization in the dashboard header.

--- app-v1/src/screens/dashboard/dashboard-header.tsx
+++ app-v1/src/screens/dashboard/dashboard-header.tsx
@@ -1,16 +1,28 @@
 import { Link } from 'expo-router';
 import { View } from 'react-native';
-import { CircleUserRound } from 'lucide-react-native';
 
-import { Icon } from '~/src/components/ui/icon';
+import { Text } from '~/src/components/ui/text';
+import { ProfileStore } from '~/src/stores/profile.store';
+import { Avatar, AvatarFallback, AvatarImage } from '~/src/components/ui/avatar';
 
 export const DashboardHeader = () => {
+  const { profile } = ProfileStore();
+
   return (
     <View className='h-16 flex-row items-center'>
       <View className='flex-1'></View>
       <View className='me-2'>
         <Link href='/profile'>
-          <Icon as={CircleUserRound} size={48} />
+          <View className='rounded-full bg-primary/25 p-1'>
+            <Avatar alt={'profile-image'} className='size-12'>
+              {profile?.image && <AvatarImage source={{ uri: profile.image }} />}
+              <AvatarFallback>
+                <Text className='font-Poppins500 text-2xl text-primary'>
+                  H
+                </Text>
+              </AvatarFallback>
+            </Avatar>
+          </View>
         </Link>
       </View>
     </View>
